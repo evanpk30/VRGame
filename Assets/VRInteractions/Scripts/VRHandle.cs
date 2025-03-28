9744dff724847df1bf87ad0ca8d65f714b5bcccf
evanpk30 <evanklaasen@gmail.com>
2025-03-27 22:02:07 -0700

    Added puzzle testing scene
    added the puzzle testing scene along with some buttons and levers, also added teleportation and fixed the hands a bit so that they have direct interactions, looking to maybe change a button to activate menu interaction mode? Or just implement that into each menu?

---
+++ Assets/VRInteractions/Scripts/VRHandle.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit;
+
+/// <summary>
+/// VR adds a grippable handle to the lever. Makes the lever attachable to the VR controller.
+/// </summary>
+public class VRHandle : MonoBehaviour
+{
+    /// <summary>
+    /// XR Interactable for the handle
+    /// </summary>
+    public XRGrabInteractable grabInteractable = new XRGrabInteractable();
+
+    /// <summary>
+    /// Object that is the spawn point of the handle joint
+    /// </summary>
+    public Transform HandlePosition;
+
+    /// <summary>
+    /// The joint that is spawned to connect the controller to the lever
+    /// The prefab can be found in the prefabs folder
+    /// </summary>
+    public Transform HandleJointPrefab;
+
+    /// <summary>
+    /// Current joint object (null if there is no connection)
+    /// </summary>
+    private Transform JointObject;
+
+    /// <summary>
+    /// Sets the breakforce of the joint that connects the controller and the lever
+    /// </summary>
+    public float breakForces = 10;
+
+    private bool bAttached = false;
+    private XRBaseInteractor currentInteractor;
+
+    void Awake()
+    {
+        // If not assigned in inspector, try to get the component
+        if (grabInteractable == null)
+        {
+            grabInteractable = GetComponent<XRGrabInteractable>();
+        }
+
+        // Set up listeners for interaction events
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.AddListener(OnGrabbed);
+            grabInteractable.selectExited.AddListener(OnReleased);
+        }
+    }
+
+    void OnGrabbed(SelectEnterEventArgs args)
+    {
+        // Store the current interactor
+        currentInteractor = args.interactorObject as XRBaseInteractor;
+
+        // Trigger haptic feedback
+
+        AddNewJoint(currentInteractor);
+    }
+
+    void OnReleased(SelectExitEventArgs args)
+    {
+        Disconnect();
+    }
+
+    public void AddNewJoint(XRBaseInteractor _interactor)
+    {
+        // Ensure we have a Rigidbody to connect
+        Rigidbody controllerBody = _interactor.transform.GetComponent<Rigidbody>();
+        if (controllerBody == null) return;
+
+        JointObject = Instantiate(HandleJointPrefab, HandlePosition.position, Quaternion.identity);
+        JointObject.parent = transform;
+
+        ConfigurableJoint cj = JointObject.GetComponent<ConfigurableJoint>();
+        if (cj != null)
+        {
+            cj.connectedBody = controllerBody;
+        }
+
+        FixedJoint fj = JointObject.GetComponent<FixedJoint>();
+        if (fj != null)
+        {
+            fj.connectedBody = GetComponent<Rigidbody>();
+        }
+
+        bAttached = true;
+    }
+
+    /// <summary>
+    /// Disconnects the controller from the lever
+    /// </summary>
+    public void Disconnect()
+    {
+        if (JointObject != null)
+        {
+            Destroy(JointObject.gameObject);
+        }
+
+        currentInteractor = null;
+        bAttached = false;
+    }
+
+    void OnDestroy()
+    {
+        // Clean up event listeners
+        if (grabInteractable != null)
+        {
+            grabInteractable.selectEntered.RemoveListener(OnGrabbed);
+            grabInteractable.selectExited.RemoveListener(OnReleased);
+        }
+    }
+}
---
+++ Assets/VRInteractions/Scripts/VRInteractable.cs
@@ -0,0 +1,152 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.XR.Interaction.Toolkit;
+
+/// <summary>
+/// Base class for interactables. Tracks whether the object is interactable or not and contains facilities for managing colliders.
+/// Collider management is useful for if you want to temporarily stop the controllers or the character from colliding with the interactable object
+/// </summary>
+public class VRInteractable : MonoBehaviour
+{
+
+    public bool Interactable = true;
+
+    /// <summary>
+    /// Requires a rigidbody
+    /// </summary>
+    private Rigidbody rb3d;
+
+    /// <summary>
+    /// Cached colliders for disabling collisions
+    /// </summary>
+    private List<Collider> mColliders3D;
+
+    void Awake()
+    {
+        UpdateColliders3D();
+        mCache = Interactable;
+    }
+
+    /// <summary>
+    /// Updates the cache of which colliders are in this object's hierarchy.
+    /// </summary>
+    public void UpdateColliders3D()
+    {
+        rb3d = gameObject.GetComponent<Rigidbody>();
+
+        mColliders3D = new List<Collider>();
+
+        if (rb3d != null)
+        {
+            mColliders3D.AddRange(rb3d.gameObject.GetComponentsInChildren<Collider>());
+            mColliders3D.Add(rb3d.gameObject.GetComponent<Collider>());
+        }
+        else
+        {
+            mColliders3D.AddRange(GetComponentsInChildren<Collider>());
+            mColliders3D.Add(GetComponent<Collider>());
+        }
+    }
+
+    /// <summary>
+    /// Ignores the colliders of the given rigidbody
+    /// </summary>
+    /// <param name="_rigidbody">Rigidbody.</param>
+    public void IgnoreColliders(Rigidbody _rigidbody)
+    {
+        Collider[] colliders = _rigidbody.GetComponentsInChildren<Collider>();
+        IgnoreColliders3D(colliders, mColliders3D.ToArray());
+    }
+
+    /// <summary>
+    /// Ignores the colliders of the given transform
+    /// </summary>
+    /// <param name="_object">Object.</param>
+    public void IgnoreColliders(Transform _object)
+    {
+        Collider[] colliders = _object.GetComponentsInChildren<Collider>();
+        IgnoreColliders3D(colliders, mColliders3D.ToArray());
+    }
+
+    /// <summary>
+    /// Removes the physics ignore for the given rigidbody
+    /// </summary>
+    /// <param name="_rigidbody">Rigidbody.</param>
+    public void RemoveIgnoreColliders(Rigidbody _rigidbody)
+    {
+        Collider[] colliders = _rigidbody.GetComponentsInChildren<Collider>();
+        IgnoreColliders3D(colliders, mColliders3D.ToArray(), false);
+    }
+
+    /// <summary>
+    /// Removes the physics ignore for the given transform
+    /// </summary>
+    /// <param name="_object">Object.</param>
+    public void RemoveIgnoreColliders(Transform _object)
+    {
+        Collider[] colliders = _object.GetComponentsInChildren<Collider>();
+        IgnoreColliders3D(colliders, mColliders3D.ToArray(), false);
+    }
+
+    /// <summary>
+    /// Allows prevention of collision between this object and your controllers colliders
+    /// </summary>
+    /// <param name="_colliders">Colliders of object we want to ignore collisions with</param>
+    /// <param name="_otherColliders">Colliders to ignore</param>
+    /// <param name="_ignore">Whether to ignore or restore collisions</param>
+    public static void IgnoreColliders3D(Collider[] _colliders, Collider[] _otherColliders, bool _ignore = true)
+    {
+        foreach (Collider col in _colliders)
+        {
+            foreach (Collider otherCol in _otherColliders)
+            {
+                if (otherCol == null || col == null)
+                {
+                    continue;
+                }
+
+                Physics.IgnoreCollision(col, otherCol, _ignore);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Ignores colliders for all of the active XR controllers
+    /// </summary>
+    void IgnoreAllControllerColliders()
+    {
+        foreach (XRBaseController controller in VRGripper.GetControllers())
+        {
+            IgnoreColliders(controller.transform);
+        }
+    }
+
+    /// <summary>
+    /// Removes the physics ignore for the XR controllers
+    /// </summary>
+    void RemoveIgnoreAllControllerColliders()
+    {
+        foreach (XRBaseController controller in VRGripper.GetControllers())
+        {
+            RemoveIgnoreColliders(controller.transform);
+        }
+    }
+
+    /// <summary>
+    /// Tracks changes to interactable state
+    /// </summary>
+    private bool mCache;
+
+    public void Update()
+    {
+        if (mCache != Interactable)
+        {
+            if (!Interactable)
+                IgnoreAllControllerColliders();
+            else
+                RemoveIgnoreAllControllerColliders();
+
+            mCache = Interactable;
+        }
+    }
+}
